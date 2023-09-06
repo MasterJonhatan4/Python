ec495a2b4d264f2f5cf9260154e8c9547dda99ae
MasterJonhatan4 <132089093+MasterJonhatan4@users.noreply.github.com>
2023-09-06 23:02:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+"""LOGIC"""
+
+def neg (p):
+    return  not p
+
+
+def conj(p,q):
+    if p == True and q == True:
+        return True
+    else:
+        return False
+    
+
+def disj (p,q):
+    if p == True or q == True:
+        return True
+    else:
+        return False
+    
+def impl (p,q):
+    if p == True and q == False:
+        return False
+    else:
+        return True
+    
+
+def equi(p,q):
+    if p == q :
+        return True
+    else:
+        return False
+
+
+def exdisj(p,q):
+    if p == q:
+        return False
+    else:
+        return True
+
+p = False 
+q = False
+print("p= " + str (p) )
+print("q= " + str (q) )
+
+print("conj (p,q) = " + str(conj(p,q)))
+print("disj (p,q) = " + str(disj (p,q)))
+print("impl (p,q) = "+ str(impl (p,q)))
+print("equi(p,q) = " + str(equi(p,q)))
+print()
+print("exdisj(p,q) = " + str(exdisj(p,q)))
