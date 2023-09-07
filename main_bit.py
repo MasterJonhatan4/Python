7723202ad0292045d539829d0279f04c77098e6e
MasterJonhatan4 <132089093+MasterJonhatan4@users.noreply.github.com>
2023-09-07 22:26:07 +0200

    Add files via upload

---
+++ main_bit.py
@@ -0,0 +1,36 @@
+"""BOOLEAN LOGIC"""
+
+def AND(b1,b2,b):
+    if b1 and b2 == "1":
+        b = "1"
+    else:
+        b = "0"
+    print (b1 + " AND " + b2 + " = " + b)
+    
+
+def OR (b1,b2,b):
+    if b1 or b2 == "1":
+        b = "1"
+    else:
+        b = "0"
+    print (b1 + " OR " + b2 + " = " + b)
+    
+    
+
+def XOR (b1,b2,b):
+    if b1 != b2:
+        b = "1"
+    else :
+        b = "0"
+    print (b1 + " XOR " + b2 + " = " + b)
+   
+
+b1 = "1" 
+b2 = "1"
+b = ""
+
+AND(b1,b2,b)
+
+OR(b1,b2,b)
+
+XOR(b1,b2,b)
